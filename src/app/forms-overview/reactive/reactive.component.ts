50296035f42fbc098b31872595bd45691aace594
PavanBashetty <pavankumar.rb25@gmail.com>
2024-05-02 14:51:31 +0200

    reactive form added

--- src/app/forms-overview/reactive/reactive.component.ts
+++ src/app/forms-overview/reactive/reactive.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/_services/api.service';
+import { FormsOverviewComponent } from '../forms-overview.component';
 
 @Component({
   selector: 'app-reactive',
@@ -7,4 +10,58 @@ import { Component } from '@angular/core';
 })
 export class ReactiveComponent {
 
+  public successMsg!:string;
+  studentForm: FormGroup = new FormGroup({});
+
+  constructor(private apiService:ApiService, private formBuilder:FormBuilder, private formOverViewComp:FormsOverviewComponent){}
+
+  ngOnInit(){
+    this.studentForm = this.formBuilder.group({
+      firstName:['', Validators.required],
+      lastName:['', Validators.required],
+      age:[0,Validators.required],
+      address: this.formBuilder.group({
+        street:['', Validators.required],
+        city:['', Validators.required],
+        zipcode:['', Validators.required],
+        country:['', Validators.required]
+      }),
+      aliases: this.formBuilder.array([])
+    })
+  }
+
+  get aliases(){
+    return this.studentForm.get('aliases') as FormArray;
+  }
+
+  addAlias(){
+    this.aliases.push(this.formBuilder.control(''));
+  }
+
+  removeAlias(i:number){
+    this.aliases.removeAt(i)
+  }
+
+  submitNewStudent(){
+    this.apiService.addNewStudent(this.studentForm.value).subscribe({
+      next:()=>{
+        this.successMsg = 'New student entry added successfully!!';
+        this.formOverViewComp.getStudentData();
+        this.studentForm.reset();
+        setTimeout(()=>{
+          this.successMsg = ''
+        },2500)
+      },
+      error:(error:any)=>{console.log(error)}
+    })
+  }
+
+  cancelSubmission(){
+    this.aliases.clear();
+    this.studentForm.reset();
+  }
+
+  get isFormValid():boolean{
+    return this.studentForm.valid;
+  }
 }
