85c6e1233653e87d464162334b83bd329c70dbbd
PavanBashetty <pavankumar.rb25@gmail.com>
2024-05-04 00:48:44 +0200

    rxjs operators added

--- src/app/rxjs-overview/operators/operators.component.ts
+++ src/app/rxjs-overview/operators/operators.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, debounceTime, distinctUntilChanged, map, of, switchMap, toArray } from 'rxjs';
+import { newStudent } from 'src/app/_classes/studentList';
+import { ApiService } from 'src/app/_services/api.service';
 
 @Component({
   selector: 'app-operators',
@@ -7,4 +11,48 @@ import { Component } from '@angular/core';
 })
 export class OperatorsComponent {
 
+  originalStudentList!:newStudent[];
+  mapStudentList!:newStudent[];
+  filterStudentList!:newStudent[];
+
+  searchedName = new FormControl();
+  searchResult$ !: Observable<newStudent[]>;
+
+  numberArray: number[] = [1, 2, 3, 4, 4, 5, 6, 4, 7, 8, 8, 9];
+  numberArray$ = of(...this.numberArray);
+  distinctNumberArray!: number[];
+
+  constructor(private apiService:ApiService){}
+
+  ngOnInit(){
+    this.apiService.getStudentList().subscribe({
+      next:(data:newStudent[])=>{this.originalStudentList = data},
+      error:(error:any)=>{console.log(error)}
+    });
+
+    this.apiService.getStudentList().
+    pipe(map(students => students.map(student => ({...student, age:student.age * 2}))))
+    .subscribe({
+      next:(data)=>{this.mapStudentList = data},
+      error:(error:any)=>{console.log(error)}
+    });
+
+    this.apiService.getStudentList().
+    pipe(map(students => students.filter(student=>(student.age % 2 != 0))))
+    .subscribe({
+      next:(data)=>{this.filterStudentList = data},
+      error:(error:any)=>{console.log(error)}
+    });
+
+    this.searchResult$ = this.searchedName.valueChanges.
+    pipe(debounceTime(300),switchMap(enteredName => enteredName ? this.apiService.searchStudent(enteredName) : of([])));
+
+    this.numberArray$.
+    pipe(distinctUntilChanged(), toArray()).subscribe({
+      next: (data) => { this.distinctNumberArray = data }
+    });
+    //toArray() operator collects all emitted values into an array.
+
+
+  }
 }
